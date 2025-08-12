531eaeab1837133e99fc0a3e92cddf4a1457344a
krs <k.kamasa@gmail.com>
2025-08-12 11:57:30 +0200

    Aktualizacja pliku index.js o logowanie kodu odpowiedzi weryfikacji sesji. Zmiana adresu URL w test.js na produkcyjny oraz aktualizacja tagu obrazu w values.yaml na 4.

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/verify', parser.urlencoded({ extended: false }), async (req, res) => 
   }
 
   const response = await axios.post(session.url, {...session, code}, {validateStatus: () => true});
-  
+  console.log("Session verifivation response code:", response.statusCode, session.id)
   if (response.statusCode === 200) {
     twiml.say(twimlOptions, "Kod poprawny. Trwa finalizacja transakcji.")
     session.status = "verified"
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const URL = "https://paycadoo-krs-call-challange.dev.kubeticon.com";
+const URL = "https://paycadoo-call-challange.prod.kubeticon.com";
 const TOKEN = "dasijewij84u32832ue32rd2uh43uhffq4huhgruhdgh"
-const PHONE = "+48518801987"
+const PHONE = "+48663366883"
 const CODE = "123456"
 
 const client = axios.create({
@@ -16,7 +16,7 @@ async function main() {
   const response = await client.post(`/session`, {
     phone: PHONE,
     code: CODE,
-    url: "https://wp.pl"
+    url: "https://krs.requestcatcher.com/test"
   });
   const code = response.data;
   console.log("Code created", code);
